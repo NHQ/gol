949c4a5cabe3066c4050848a6abbfba6632dedbc
NHQ SERVER ADMIN <nhq@NHQ.att.net>
2014-03-16 09:42:15 -0700

    fixed bug

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ var squarejob = require('./lib/squarejob');
 var pice = require('./lib/pices.js');
 var rgba = require('./lib/rgba.js')
 var energyProses = require('./lib/energyzer.js')
+var rectSect = require('./lib/rectIntersect')
 var w,h,draw,drawS,lifeSize,zom,data,data2,action;
 var pices = new Array();
 var tasks = new Array();
@@ -57,12 +58,13 @@ stream.on('data', function(data){
   data = decode(data)
 
   if(data.metadata.type === "ticktock"){
-  if(playflag){
-    executer(data.metadata.timestamp)
-    run()
-  }}
+    if(playflag){
+      executer(data.metadata.timestamp)
+      run()
+    }
+  }
   else if(data.metadata.type === "play"){
-    play()
+    playflag = true
     timeStamp = data.metadata.timestamp
   }
   else if(data.data){
@@ -77,13 +79,86 @@ function executer(line){
 
   var safty = 200
   var time = line - safty 
+
   temparray = tasks.filter(function(e){
     return e.metadata.timestamp >= time
   })
   tasks = tasks.filter(function(e){
     return e.metadata.timestamp < time
-  })
-  console.log(tasks.slice(), temparray.slice())
+  }).sort(function(e,i){
+    var a = 0, b = 0;
+    if((a = e.metadata.timestamp) <= (b = i.metadata.timestamp)){
+      if(a == b) return 0
+      else return -1
+    }
+    else return 1
+  }).reduce(function(p,e,i,d){
+    var x = [e];
+    if(i > 0){
+      var q = p[p.length - 1];
+      if(q[0].metadata.timestamp == e.metadata.timestamp){
+        // put all same-timestamps in same array
+        q.push(e)
+      }
+      else p.push(x)
+    }
+    else p.push(x)
+    return p
+  },[]).map(function(e, i, a){
+    if(e.length == 1) return e
+    else{
+      var box = Object.create(null)
+      box['BStemp'] = []
+      box['build'] = []
+      e.forEach(function(e){
+        if(e.metadata.type == 'BStemp') box[e.metadata.type].push(e)
+        else box['build'].push(e)
+      })
+      return box
+    }
+  }).reduce(function(_tasks,e,i,d){
+    if(Array.isArray(e)) _tasks.push(e[0])
+    else{ // should be an object
+      // iterate over the keys in e
+      Object.keys(e).forEach(function(type){
+        switch(type){
+          case 'BStemp':
+          // e[type] is an array of same-time stamps
+          // check if they are intersecting first
+          // if they are, check for contentious points
+          // of which be there any, all conflicting stamps are disregarded
+            e[type] = e[type].reduce(function(p,e,i,d){
+              var x, y;
+              var p1 = [x = e.metadata.ex, y = e.metadata.ey];
+              var p2 = [x + e.data.shape[0], y + e.data.shape[1]];
+              var intersects = false
+              var conflict = false
+              for(x = 0; x < d.length; x++){
+                var p3 = [d[x].metadata.ex, d[x].metadata.ey]
+                var p4 = [p3[0] + d[x].data.shape[0], p3[1] + p[3].data.shape[1]]
+                if(rectSect(p1, p2, p4, p4)){
+                  // we have an intersection
+                  // the next step would be to see if they contend for individual coordinates
+                  // and if not, let them through
+                  // ie. calculate rectangle intersection, and compare each coordinate
+                  // but for now we will throw out all intersecting, same time stamos
+                  intersects = true
+                }
+                else continue
+              }
+              if(!intersects) _tasks.push(e) 
+            },[])
+          break;
+          case  'build':
+          _tasks.push(e)
+          //e[type] is an array of same-time buildings
+          break;
+        }
+      })
+    }
+    return _tasks
+  },[])
+//  console.log(tasks.slice(), temparray.slice())
 
   for(work = 0 ; work < tasks.length;work++){ 
     switch(tasks[work].metadata.type){
@@ -132,12 +207,6 @@ function init(){
   drawGrid(drawS, 200, 200, stempSize)
 }
 function run(evt){
-   /* tempTasks = tasks.splice(0,tasks.length-1)
-    for(work = 0 ; work < tempTasks.length; work ++ ){
-    tempTasks[work]
-
-    }
-  */
   rules(prev, next)
   squarejob(next, draw, lifeSize)
   tern ++
@@ -168,13 +237,9 @@ function run(evt){
 var ePoint 
 ui.stemps.addEventListener('touchdown',drawStemp)
 ui.board.addEventListener('touchdown', function (e){
-ePoint = e
-command(action,ePoint)
-
-
-
-
-} )
+  ePoint = e
+  command(action,ePoint)
+})
 var timeStamp
 ui.stop.addEventListener('touchdown', function(){
 //  window.cancelAnimationFrame(anim)
@@ -261,16 +326,6 @@ var next = ndarray(data2, [Math.ceil(w / lifeSize), Math.ceil(h / lifeSize)]);
 var stemp = ndarray(data3, [Math.ceil(200 / 40), Math.ceil(200 / 40)]);
 ///var stempS = ndarray(data3, [Math.ceil(123 / 40), Math.ceil(267 / 40)]);
 
-window.addEventListener('resize', function(evt){
-//  init()
-//  squarejob(prev, draw, lifeSize)
-//  for(var i = 0; i < next.shape[0]; i++){
-//    for(var j = 0; j < next.shape[1]; j++){
-//      var n = prev.get(i, j)
-//      gen(i * lifeSize, j * lifeSize, n - 10)
-//    }
-//  }
-}, false)
 
   var pixel = draw.getImageData(0,0,100,100)
 
@@ -325,86 +380,77 @@ function play(t){
   //run()
 }
 function builder(ex,ey,type,team){
-var energyPross
-if(pice.ICanBuild(ex,ey,team,pices)){
-  energyPross = energyProses(pices,team,ex,ey,5,"build")
-  if(energyPross[0]){
-    pices = energyPross[1];
-    pices.push(new pice.set(type,ex,ey,team,pices))
+  var energyPross
+  if(pice.ICanBuild(ex,ey,team,pices)){
+    energyPross = energyProses(pices,team,ex,ey,5,"build")
+    if(energyPross[0]){
+      pices = energyPross[1];
+      pices.push(new pice.set(type,ex,ey,team,pices))
+    }
+    else
+      console.log("you do not have enugh energy");
   }
-  else
-    console.log("you do not have enugh energy");
-}
-
-
 }
 
 
 
 function springStemp(ex,ey,team,Tstemp){
-var precheak = new Array();
-var energy
-var tempPices = pices.slice(0)
-var obs = 0;
-var n = 0;
-var zbord , zstemp, x, y;
+  var precheak = new Array();
+  var energy
+  var tempPices = pices.slice(0)
+  var obs = 0;
+  var n = 0;
+  var zbord , zstemp, x, y;
 
 
   for(i = 0 ; i < Tstemp.shape[0] ; i++){      //// run therow all stemp 2d array
-  for(j = 0 ; j < Tstemp.shape[1] ; j++){
-   zstemp = Tstemp.get( i,  j)
-   if(zstemp === 0) {           /// if ther is a cell in the stemp 
-     x = ex + i
-     y = ey + j
-     zbord = prev.get(x,y)
-     if(zbord === 100){
-  energy =  energyProses(pices,team,x,y,1,"spun")
-
-     if(energy[0]) {
-  tempPices = energy[1]
-
-     }
-     else obs = 100;
+    for(j = 0 ; j < Tstemp.shape[1] ; j++){
+      zstemp = Tstemp.get( i,  j)
+      if(zstemp === 0) {           /// if ther is a cell in the stemp 
+        x = ex + i
+        y = ey + j
+        zbord = prev.get(x,y)
+        if(zbord === 100){
+          energy =  energyProses(pices,team,x,y,1,"spun")
+          if(energy[0]) {
+            tempPices = energy[1]
+          }
+          else obs = 100;
+          }
+      else obs ++
     }
-     else obs ++
-   }
-   precheak[n] = zstemp
-   n++
-
+    precheak[n] = zstemp
+    n++
+    }
   }
-  }
-  if(obs === 0)
-  {
+  if(obs === 0){
+    n = 0
+    for(i = 0 ; i < Tstemp.shape[0] ; i++){
+      for(j = 0 ; j < Tstemp.shape[1] ; j++){
+        if(precheak[n] != 100){
+          x = ex + i
+          y = ey + j
+          prev.set(x, y, team )
+          next.set(x, y, team )  //////// dump the info from stemps  
 
-n = 0
-   for(i = 0 ; i < Tstemp.shape[0] ; i++){
-   for(j = 0 ; j < Tstemp.shape[1] ; j++){
-   if(precheak[n] != 100){
-     x = ex + i
-     y = ey + j
-     prev.set(x, y, team )
-     next.set(x, y, team )  //////// dump the info from stemps  
-
-     }
-     n++
-   }
-   }
-   pices = tempPices
+        }
+         n++
+      }
+    }
+    pices = tempPices
   }
   else if(obs<100)
     console.log("cant create", obs, "obsticles");
   else if(obs === 200)
-  console.log("not enugh energy")
+    console.log("not enugh energy")
   else
     console.log("out of your inflouens fild");
-
 }
 
 function drawStemp(e){
   var x = e.detail.x, y = e.detail.y;
   x -= e.srcElement.offsetLeft
   y -= e.srcElement.offsetTop
-  console.log(x,y)
   x -= x % stempSize
   y -= y % stempSize
   x /= stempSize
@@ -414,18 +460,14 @@ function drawStemp(e){
   else z = 100
   stemp.set(x,y,z)
   squarejob(stemp,drawS,stempSize)
-  }
+}
 
-
-
+/*
 var screen = fs(document.body);
-
-screen.on('attain', function(){
-})
-
+screen.on('attain', function(){})
 screen.on('error', function(e){console.log(e)})
 
 document.body.addEventListener('click', function(){
 //  screen.request()
 })
-
+*/
